042e7a0052ecd669da0d7d4a43b3d04127781477
AbhinavMangalore16 <abhinavm16104@gmail.com>
2025-03-09 14:37:11 +0530

    backend setup done

---
+++ backend/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name='Niramay_backend',
+    version='0.1.0',
+    author='Abhinav Mangalore',
+    author_email='phoenixrfta1614@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
